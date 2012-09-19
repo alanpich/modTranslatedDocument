8962b8b7cb979c058abd8e2197256d3d3a2064ee
Alan Pich <alan.pich@gmail.com>
2012-09-19 16:16:59 +0100

    Wrapped getBrowserLanguage for safety

--- core/components/translations/elements/plugins/translations.plugin.php
+++ core/components/translations/elements/plugins/translations.plugin.php
@@ -19,30 +19,31 @@
 **/
 //==============================================================================
 //==============================================================================
-
-function getBrowserLanguage(){
-		$langs = array();
-		if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-			// break up string into pieces (languages and q factors)
-			preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i',
-					$_SERVER['HTTP_ACCEPT_LANGUAGE'], $lang_parse);
-			if (count($lang_parse[1])) {
-				// create a list like "en" => 0.8
-				$langs = array_combine($lang_parse[1], $lang_parse[4]);
-				// set default to 1 for any without q factor
-				foreach ($langs as $lang => $val) {
-					if ($val === '') $langs[$lang] = 1;
+if( !function_exists('translations_getBrowserLanguage') ){
+	function translations_getBrowserLanguage(){
+			$langs = array();
+			if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+				// break up string into pieces (languages and q factors)
+				preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i',
+						$_SERVER['HTTP_ACCEPT_LANGUAGE'], $lang_parse);
+				if (count($lang_parse[1])) {
+					// create a list like "en" => 0.8
+					$langs = array_combine($lang_parse[1], $lang_parse[4]);
+					// set default to 1 for any without q factor
+					foreach ($langs as $lang => $val) {
+						if ($val === '') $langs[$lang] = 1;
+					}
+					// sort list based on value	
+					arsort($langs, SORT_NUMERIC);
 				}
-				// sort list based on value	
-				arsort($langs, SORT_NUMERIC);
 			}
-		}
-		//extract most important (first)
-		foreach ($langs as $lang => $val) { break; }
-		//if complex language simplify it
-		if (stristr($lang,"-")) {$tmp = explode("-",$lang); $lang = $tmp[0]; }
-		return $lang;
-	};//
+			//extract most important (first)
+			foreach ($langs as $lang => $val) { break; }
+			//if complex language simplify it
+			if (stristr($lang,"-")) {$tmp = explode("-",$lang); $lang = $tmp[0]; }
+			return $lang;
+		};//
+};
 
 //==============================================================================
 //==============================================================================
